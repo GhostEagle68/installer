c6061ef4565638771387fc842deb4ede05c0144f
GhostEagle68 <Ghosteagle68@gmail.com>
2021-11-28 14:16:39 -0700

    update track text color

--- src/renderer/components/AircraftSection/TrackSelector.tsx
+++ src/renderer/components/AircraftSection/TrackSelector.tsx
@@ -56,8 +56,8 @@ export const Track: React.FC<TrackProps> = ({ isSelected, isInstalled, handleSel
             <div
                 className={`${makeBorderStyle()} w-1 h-12 rounded-r-xl transition-all duration-200 transform ${isSelected ? 'scale-y-100' : 'scale-y-50'}`}/>
             <div className="flex flex-col px-5 py-2">
-                <span className={`text-xl ${isInstalled ? '' : ''} ${isSelected ? 'text-blue-cyan' : 'text-gray-50'}`}>{track.name}</span>
-                <span className={`text-2x1 ${isInstalled ? '' : ''} ${isSelected ? 'text-blue-cyan' : 'text-gray-50'}`}><code>{latestVersionName}</code></span>
+                <span className={`text-xl ${isInstalled ? '' : ''} ${isSelected ? 'text-blue-cyan' : 'text-teal-50'}`}>{track.name}</span>
+                <span className={`text-2x1 ${isInstalled ? '' : ''} ${isSelected ? 'text-blue-cyan' : 'text-teal-50'}`}><code>{latestVersionName}</code></span>
             </div>
             {makeInstalledCheckmark()}
         </div>
